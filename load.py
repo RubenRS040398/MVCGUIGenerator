4775ea33faa0a7c3327dde6f148444bcf735f8b7
Rubén Ramos Segarra <57227280+RubenRS040398@users.noreply.github.com>
2025-04-25 16:59:43 +0200

    Added load_train_data() function, fixed merge() function

--- load.py
+++ load.py
@@ -1,16 +1,33 @@
 import os
+import pandas as pd
 
-def load():
+def load_source_code():
     folder = os.path.join(os.getcwd(), 'code')
     file_names = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
     source_code = merge(folder, file_names)
     return source_code
 
-def merge(folder, file_names):
-    merged_code = ""
-    for file_name in os.listdir(folder):
+def merge(folder, file_names, data=False):
+    merged = None
+    if not data:
+        merged = ""
+    else:
+        merged = pd.DataFrame()
+    for file_name in file_names:
         file_path = os.path.join(folder, file_name)
-        if os.path.isfile(file_path) and file_name != "view.py":
-            with open(file_path, "r", encoding="utf-8") as file:
-                merged_code += file.read()
-    return merged_code
+        if not data:
+            if os.path.isfile(file_path) and file_name != "view.py":
+                with open(file_path, "r", encoding="utf-8") as file:
+                    merged += file.read()
+        else:
+            if os.path.isfile(file_path):
+                print(file_path)
+                dataframe = pd.read_excel(file_path)
+                merged = pd.concat([dataframe])
+    return merged
+
+def load_train_data():
+    folder = os.path.join(os.getcwd(), 'data')
+    file_names = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+    train_data = merge(folder, file_names, True)
+    return train_data
